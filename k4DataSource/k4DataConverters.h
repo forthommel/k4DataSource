b56dbaa22ffb05b515315d9d1286a7435c0621e3
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-08-29 10:24:30 +0200

    Improved documentation for DataConverters LUT

--- k4DataSource/k4DataConverters.h
+++ k4DataSource/k4DataConverters.h
@@ -15,35 +15,40 @@
   };                                                                           \
   static const BUILDERNM(obj) gk4DataConverter##obj;
 
+/// A (singleton) lookup table with holding a "name" -> data formatter object constructor mapping
 class k4DataConverters {
 public:
+  /// Get a singleton of the data formatters lookup table
   static k4DataConverters& get();
 
   k4DataConverters(const k4DataConverters&) = delete;
   virtual ~k4DataConverters() = default;
   void operator=(k4DataConverters&) = delete;
 
+  /// List of data formatters registered in the lookup table
   std::vector<std::string> converters() const;
-
+  /// Register a data formatter into the singleton lookup table
   template <typename T>
-  void registerConverter(const std::string& name) {
+  inline void registerConverter(const std::string& name) {
     static_assert(std::is_base_of<DataFormatter, T>::value,
                   "\n\n  *** Failed to register an object with improper inheritance into the factory. ***\n");
     converters_[name] = &build<T>;
   }
-
+  /// Build a data formatter with a given name
   std::unique_ptr<DataFormatter> build(const std::string& name) const;
 
 private:
   k4DataConverters() {}
 
+  /// Recipe to build a data formatter
   template <typename T>
-  static std::unique_ptr<DataFormatter> build() {
+  static inline std::unique_ptr<DataFormatter> build() {
     return std::unique_ptr<DataFormatter>(new T);
   }
 
+  /// Mockup of a data formatter constructor
   typedef std::unique_ptr<DataFormatter> (*Converter)();
-
+  /// A collection of data formatters builders
   std::unordered_map<std::string, Converter> converters_;
 };
 
