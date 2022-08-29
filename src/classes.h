06607eb34eba475ed3a6f98e1e3172f888f6a88f
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-08-29 23:34:23 +0200

    Added some useful vector definitions for readout

--- src/classes.h
+++ src/classes.h
@@ -1,1 +1,9 @@
+#include <edm4hep/ClusterData.h>
+#include <edm4hep/MCParticleData.h>
+#include <edm4hep/MCRecoParticleAssociationData.h>
+#include <edm4hep/ParticleIDData.h>
+#include <edm4hep/ReconstructedParticleData.h>
+#include <edm4hep/TrackData.h>
+#include <edm4hep/TrackState.h>
+
 #include "k4DataSource/k4DataSource.h"
