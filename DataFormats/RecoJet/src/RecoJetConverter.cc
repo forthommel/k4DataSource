6be7ff0a1717d9460235cf0ab66ef95bc9aac210
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-08-31 10:04:25 +0200

    DataFormats: jets producer now takes reconstructed particles as input

--- DataFormats/RecoJet/src/RecoJetConverter.cc
+++ DataFormats/RecoJet/src/RecoJetConverter.cc
@@ -1,8 +1,7 @@
 #include <edm4hep/ReconstructedParticleData.h>
 
-#include <iostream>  //FIXME
-
 #include "DataFormats/RecoJet/include/RecoJet.h"
+#include "DataFormats/RecoParticle/include/RecoParticle.h"
 #include "k4DataSource/k4DataConverter.h"
 #include "k4DataSource/k4DataConverterFactory.h"
 
@@ -10,7 +9,7 @@ class RecoJetsConverter : public k4DataConverter {
 public:
   RecoJetsConverter()
       : h_jets_(consumes<std::vector<edm4hep::ReconstructedParticleData> >("Jet")),
-        h_reco_parts_(consumes<std::vector<edm4hep::ReconstructedParticleData> >("ReconstructedParticles")) {
+        h_reco_parts_(consumes<std::vector<RecoParticle> >("RecoParticles")) {
     produces<RecoJet>("RecoJet");
   }
 
@@ -18,17 +17,17 @@ public:
     auto output = std::make_unique<std::vector<RecoJet> >();
     for (const auto& jet : *h_jets_) {
       auto& out_jet = output->emplace_back(jet.momentum.x, jet.momentum.y, jet.momentum.z, jet.energy);
-      for (auto it = jet.particles_begin; it < jet.particles_end; ++it) {
-        const auto& jc = h_reco_parts_->at(it);
-        out_jet.addConstituent(RecoParticle(jc.momentum.x, jc.momentum.y, jc.momentum.z, jc.energy));
-      }
+      // add in jets constituents
+      //FIXME work with references instead of copies
+      for (auto it = jet.particles_begin; it < jet.particles_end; ++it)
+        out_jet.addConstituent(h_reco_parts_->at(it));
     }
     put(std::move(output));
   }
 
 private:
   Handle<std::vector<edm4hep::ReconstructedParticleData> > h_jets_;
-  Handle<std::vector<edm4hep::ReconstructedParticleData> > h_reco_parts_;
+  Handle<std::vector<RecoParticle> > h_reco_parts_;
 };
 
 REGISTER_CONVERTER("RecoJets", RecoJetsConverter)
