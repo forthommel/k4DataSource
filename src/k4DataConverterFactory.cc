7900235a1407111432501602e29cfb65179b17be
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-09-03 00:46:55 +0200

    Core: exception thrown if converter is not found

--- src/k4DataConverterFactory.cc
+++ src/k4DataConverterFactory.cc
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include "k4DataSource/k4DataConverterFactory.h"
 
 k4DataConverterFactory& k4DataConverterFactory::get() {
@@ -13,5 +15,7 @@ std::vector<std::string> k4DataConverterFactory::converters() const {
 }
 
 std::unique_ptr<k4DataConverter> k4DataConverterFactory::build(const std::string& name) const {
+  if (converters_.count(name) == 0)
+    throw std::runtime_error("Failed to build a data converter with name '" + name + "'.");
   return converters_.at(name)();
 }
