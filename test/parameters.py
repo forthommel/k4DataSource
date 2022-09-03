76df7d42a3014b385f154cf0583da6ff6d237113
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-09-03 13:47:43 +0200

    Core: updated parameters test

--- test/parameters.py
+++ test/parameters.py
@@ -1,9 +1,22 @@
 import Core as fcc
+import unittest
 
-test = fcc.Module('test')
-print(test)
+import ROOT
 
-electrons = fcc.Converter('RecoParticles',
-    src = 'ReconstructedParticles',
-)
-print(electrons)
+class TestConverter(unittest.TestCase):
+    def testAttributes(self):
+        electrons = fcc.Converter('RecoParticles',
+            src = 'ReconstructedParticles',
+            output = 'electrons',
+            foo = 42,
+            bar = True,
+            baz = 3.1415,
+        )
+        params = electrons.parameters()
+        self.assertEqual(params.get[ROOT.std.string]('src'), 'ReconstructedParticles')
+        self.assertEqual(params.get[ROOT.std.string]('output'), 'electrons')
+        self.assertEqual(params.get[int]('foo'), 42)
+        self.assertEqual(params.get[int]('bar'), True) #FIXME
+        self.assertEqual(params.get[ROOT.double]('baz'), 3.1415)
+
+unittest.main()
