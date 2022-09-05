14a2ac6d856d6b5ef456bcdff7c77a83a23d36fa
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-09-05 12:53:48 +0200

    Core: fixed parameters test

--- test/parameters.py
+++ test/parameters.py
@@ -12,11 +12,10 @@ class TestConverter(unittest.TestCase):
             bar = True,
             baz = 3.1415,
         )
-        params = electrons.parameters()
-        self.assertEqual(params.get[ROOT.std.string]('src'), 'ReconstructedParticles')
-        self.assertEqual(params.get[ROOT.std.string]('output'), 'electrons')
-        self.assertEqual(params.get[int]('foo'), 42)
-        self.assertEqual(params.get[int]('bar'), True) #FIXME
-        self.assertEqual(params.get[ROOT.double]('baz'), 3.1415)
+        self.assertEqual(electrons.get[ROOT.std.string]('src'), 'ReconstructedParticles')
+        self.assertEqual(electrons.get[ROOT.std.string]('output'), 'electrons')
+        self.assertEqual(electrons.get[int]('foo'), 42)
+        self.assertEqual(electrons.get[int]('bar'), True) #FIXME
+        self.assertEqual(electrons.get[ROOT.double]('baz'), 3.1415)
 
 unittest.main()
