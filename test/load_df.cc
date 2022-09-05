dc3f9a2b19a387d932c944b5158f8de047dd6653
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-09-05 10:05:10 +0200

    Core: added MT case for DF test

--- test/load_df.cc
+++ test/load_df.cc
@@ -9,3 +9,11 @@ TEST_CASE("k4DataFrame", "[basics]") {
                 ->Range(0, 1000);
   df.Snapshot("events", "test.root", {"recoParticles", "jets"});
 }
+
+TEST_CASE("k4DataFrameMT", "[basics]") {
+  ROOT::EnableImplicitMT();
+  auto df = MakeK4DataFrame({"root://eospublic.cern.ch//eos/experiment/fcc/ee/generation/DelphesEvents/spring2021/IDEA/"
+                             "p8_ee_ZH_ecm240/events_101027117.root"},
+                            std::vector<std::string>{"RecoParticles", "RecoJets"})
+                ->Snapshot("events", "test.root", {"recoParticles", "jets"});
+}
