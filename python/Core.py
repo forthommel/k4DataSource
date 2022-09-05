1f9a2e401ae94b014a11b71a59936ec6d0b85bdc
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-09-05 12:36:21 +0200

    Core: updated python scripting procedure to handle converter outputs names

--- python/Core.py
+++ python/Core.py
@@ -3,9 +3,33 @@ import ROOT
 ROOT.gSystem.Load('libk4DataSource')
 _ds = ROOT.k4DataSource
 
-def Converter(name, *args, **kwargs):
-    params = ROOT.k4Parameters()
-    params.setName(name)
+class Process(object):
+    df = None
+    inputs = []
+    sources = dict()
+
+    def __init__(self, *args, **kwargs):
+        self.inputs = kwargs.get('inputs', [])
+        pass
+
+    def __setattr__(self, attr_name, attr_value):
+        if type(attr_value) == ROOT.k4Parameters:
+            self.sources[attr_name] = attr_value
+        else:
+            return super(Process, self).__setattr__(attr_name, attr_value)
+
+    def df(self):
+        sources = []
+        for k, v in self.sources.items():
+            sources.append(v.set[str]('output', k))
+        self.df = ROOT.MakeK4DataFrame(self.inputs, sources)
+        return self.df
+
+def Converter(name, **kwargs):
+    parameters = ROOT.k4Parameters()
+    if not parameters:
+        raise RuntimeError('Failed to initialise parameters list for converter "{}"'.format(name))
+    parameters.setName(name)
     for k, v in kwargs.items():
-        params.set(k, v)
-    return ROOT.k4DataConverterFactory.get().build(params)
+        parameters.set(k, v)
+    return parameters
--- test/load_df.py
+++ test/load_df.py
@@ -1,18 +1,25 @@
 import Core as fcc
 import ROOT
 
-files = ['root://eospublic.cern.ch//eos/experiment/fcc/ee/generation/DelphesEvents/spring2021/IDEA/p8_ee_ZH_ecm240/events_101027117.root']
 
-electrons = fcc.Converter('RecoParticles',
-    src = 'ReconstructedParticles',
-    output = 'electrons',
+process = fcc.Process(
+    inputs = ['root://eospublic.cern.ch//eos/experiment/fcc/ee/generation/DelphesEvents/spring2021/IDEA/p8_ee_ZH_ecm240/events_101027117.root'],
 )
 
-df = ROOT.MakeK4DataFrame(files, [electrons])
+process.electrons = fcc.Converter('RecoParticles',
+    src = 'ReconstructedParticles',
+)
+process.recoParticles = fcc.Converter('RecoParticles')
+process.jets = fcc.Converter('RecoJets',
+    src = 'Jet',
+)
+
+#ROOT.EnableImplicitMT()
 
 branches = ROOT.vector('string')()
-for br in ['electrons']:
+for br in ['electrons', 'jets', 'recoParticles']:
     branches.push_back(br)
 
-df = df.Range(0, 10)
+#df = process.df()
+df = process.df().Range(0, 10)
 df.Snapshot('events', 'test.root', branches)
