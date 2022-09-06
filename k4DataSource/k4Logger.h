211fe8b7fdde66bdf7f84776e5bb4f11ba6e458c
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-09-06 10:02:08 +0200

    Core: added intermediate logger object

--- k4DataSource/k4Logger.h
+++ k4DataSource/k4Logger.h
@@ -8,30 +8,49 @@
 #include <string>
 #include <vector>
 
-extern std::mutex gMutex;
-
+/// A (singleton) logger object
 class k4Logger {
 public:
-  explicit k4Logger(const std::string&, const std::string&, size_t);
-  k4Logger(const k4Logger&);
-  virtual ~k4Logger();
+  /// Get a singleton of the messages logger
+  static k4Logger& get(std::ostream* = nullptr);
 
+  k4Logger(const k4Logger&) = delete;
+  virtual ~k4Logger() = default;
+  void operator=(k4Logger&) = delete;
+
+  std::ostream* stream();
+
+  static std::mutex mutex;
+  static std::string now(const std::string& = "%H:%M:%S");
+
+private:
+  k4Logger(std::ostream* = nullptr);
+  std::ostream* stream_{nullptr};
+};
+
+class k4Message {
+public:
+  explicit k4Message(const std::string&, const std::string&, size_t) noexcept;
+  k4Message(const k4Message&) noexcept;
+  virtual ~k4Message() noexcept;
+
+  void dump(std::ostream* = nullptr) const noexcept;
   std::string message() const;
 
   template <typename T>
-  inline friend const k4Logger& operator<<(const k4Logger& log, const T& var) noexcept {
-    auto& nc_log = const_cast<k4Logger&>(log);
+  inline friend const k4Message& operator<<(const k4Message& log, const T& var) noexcept {
+    auto& nc_log = const_cast<k4Message&>(log);
     nc_log.stream_ << var;
     return log;
   }
 
   template <typename T, typename U>
-  inline friend const k4Logger& operator<<(const k4Logger& log, const std::pair<T, U>& pair_var) noexcept {
+  inline friend const k4Message& operator<<(const k4Message& log, const std::pair<T, U>& pair_var) noexcept {
     return log << "(" << pair_var.first << ", " << pair_var.second << ")";
   }
 
   template <typename T>
-  inline friend const k4Logger& operator<<(const k4Logger& log, const std::vector<T>& vec_var) noexcept {
+  inline friend const k4Message& operator<<(const k4Message& log, const std::vector<T>& vec_var) noexcept {
     log << "{";
     std::string sep;
     for (const auto& var : vec_var)
@@ -40,7 +59,7 @@ public:
   }
 
   template <typename T, typename U>
-  inline friend const k4Logger& operator<<(const k4Logger& log, const std::map<T, U>& map_var) noexcept {
+  inline friend const k4Message& operator<<(const k4Message& log, const std::map<T, U>& map_var) noexcept {
     log << "{";
     std::string sep;
     for (const auto& var : map_var)
@@ -48,40 +67,41 @@ public:
     return log << "}";
   }
 
-  inline friend const k4Logger& operator<<(const k4Logger& log, std::ios_base& (*f)(std::ios_base&)) noexcept {
-    auto& nc_log = const_cast<k4Logger&>(log);
+  inline friend const k4Message& operator<<(const k4Message& log, std::ios_base& (*f)(std::ios_base&)) noexcept {
+    auto& nc_log = const_cast<k4Message&>(log);
     f(nc_log.stream_);
     return log;
   }
 
   template <typename T>
-  inline const k4Logger& log(T&& lam) noexcept {
+  inline const k4Message& log(T&& lam) noexcept {
     lam(*this);
     return *this;
   }
 
-private:
-  static std::string now(const std::string& = "%H:%M:%S");
-
+protected:
   const std::string from_, file_;
   const size_t line_num_;
   std::ostringstream stream_;
 };
 
-struct k4Exception : public k4Logger, public std::runtime_error {
-  explicit k4Exception(const std::string&, const std::string&, size_t);
+struct k4Exception : public k4Message, public std::runtime_error {
+  explicit k4Exception(const std::string&, const std::string&, size_t) noexcept;
+  virtual ~k4Exception() noexcept override;
+
+  const char* what() const noexcept override;
 };
 
 struct k4EmptyLogger {
-  k4EmptyLogger() = default;
-  ~k4EmptyLogger() = default;
+  k4EmptyLogger() noexcept = default;
+  ~k4EmptyLogger() noexcept = default;
 
   template <class T>
-  k4EmptyLogger& operator<<(const T&) {
+  k4EmptyLogger& operator<<(const T&) noexcept {
     return *this;
   }
   template <typename T>
-  k4EmptyLogger& log(T&&) {
+  k4EmptyLogger& log(T&&) noexcept {
     return *this;
   }
 };
@@ -92,8 +112,8 @@ struct k4EmptyLogger {
 #define __FUNC__ __PRETTY_FUNCTION__
 #endif
 
-#define k4Log k4Logger(__FUNC__, __FILE__, __LINE__)
-#define k4Error k4Log
+#define k4Log k4Message(__FUNC__, __FILE__, __LINE__)
+#define k4Error k4Exception(__FUNC__, __FILE__, __LINE__)
 #ifdef DEBUG
 #define k4Debug k4Log
 #else
--- src/k4Logger.cc
+++ src/k4Logger.cc
@@ -3,28 +3,62 @@
 
 #include "k4DataSource/k4Logger.h"
 
-std::mutex gMutex;
+//---------------------------------------------------------
 
-k4Logger::k4Logger(const std::string& from, const std::string& file, size_t line_num)
-    : from_(from), file_(file), line_num_(line_num) {}
+std::mutex k4Logger::mutex;
 
-k4Logger::k4Logger(const k4Logger& oth)
-    : from_(oth.from_), file_(oth.file_), line_num_(oth.line_num_), stream_(oth.stream_.str()) {}
-
-k4Logger::~k4Logger() {
-  std::lock_guard<std::mutex> guard(gMutex);
-  std::cout << "[" << now() << "] " << from_ << " (" << file_ << ":" << line_num_ << ") " << stream_.str() << std::endl;
+k4Logger& k4Logger::get(std::ostream* os) {
+  static k4Logger logger(os);
+  return logger;
 }
 
-std::string k4Logger::message() const { return stream_.str(); }
+k4Logger::k4Logger(std::ostream* os) : stream_(os) {
+  if (!stream_)
+    stream_ = &std::cout;
+}
+
+std::ostream* k4Logger::stream() {
+  if (!stream_)
+    throw std::runtime_error("Failed to initialise output stream for logger instance.");
+  return stream_;
+}
 
 std::string k4Logger::now(const std::string& fmt) {
   auto now = std::time(nullptr);
   auto tm = *std::localtime(&now);
   char out_str[50];
-  strftime(out_str, 50, fmt.c_str(), &tm);
+  std::strftime(out_str, 50, fmt.c_str(), &tm);
   return std::string(out_str);
 }
 
-k4Exception::k4Exception(const std::string& from, const std::string& file, size_t line_num)
-    : k4Logger(from, file, line_num), std::runtime_error(from) {}
+//---------------------------------------------------------
+
+k4Message::k4Message(const std::string& from, const std::string& file, size_t line_num) noexcept
+    : from_(from), file_(file), line_num_(line_num) {}
+
+k4Message::k4Message(const k4Message& oth) noexcept
+    : from_(oth.from_), file_(oth.file_), line_num_(oth.line_num_), stream_(oth.stream_.str()) {}
+
+k4Message::~k4Message() noexcept { dump(); }
+
+void k4Message::dump(std::ostream* os) const noexcept {
+  if (!os)
+    os = k4Logger::get().stream();
+  std::lock_guard<std::mutex> guard(k4Logger::get().mutex);
+  (*os) << "[" << k4Logger::get().now() << "] " << from_ << " (" << file_ << ":" << line_num_ << ") " << stream_.str()
+        << std::endl;
+}
+
+std::string k4Message::message() const { return stream_.str(); }
+
+//---------------------------------------------------------
+
+k4Exception::k4Exception(const std::string& from, const std::string& file, size_t line_num) noexcept
+    : k4Message(from, file, line_num), std::runtime_error("k4Exception") {}
+
+k4Exception::~k4Exception() noexcept {}
+
+const char* k4Exception::what() const noexcept {
+  dump();
+  return "k4Exception";
+}
